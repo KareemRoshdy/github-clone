47e28e8f94ed15f91fba41ea523425cf7c0d0049
KareemRoshdy <karem22roshdy@gmail.com>
2023-10-26 22:27:38 +0200

    final upload

--- app/components/Productivity/Productivity.tsx
+++ app/components/Productivity/Productivity.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 import { useState } from "react";
 import { motion } from "framer-motion";
 import CodeEditor from "./CodeEditor";
 import CodeSpace from "./CodeSpace";
 import CopilotFeature from "./CopilotFeature";
+import HoverCard from "./HoverCard";
 
 const Productivity = () => {
   const [hovered, setHovered] = useState<boolean>(false);
@@ -77,6 +79,148 @@ const Productivity = () => {
       <div className="relative z-[1]">
         <CopilotFeature />
       </div>
+
+      <div className="flex justify-between items-center">
+        <div className="flex justify-between md:space-x-10 max-md:flex-col">
+          <HoverCard backgroundColor="#7ee787" direction="flex-col" left="0">
+            <div className="md:flex flex-col flex-1 p-8 sm:p-10 lg:py-16 lg:pl-16 lg:pr-32 ">
+              <p className=" text-xl md:text-2xl mb-6 font-medium text-[#7d8590]">
+                <span className="text-white font-semibold">GitHub Actions</span>{" "}
+                automates your build, test, and deployment workflow with simple
+                and secure CI/CD.
+              </p>
+
+              <div>
+                <a
+                  onMouseEnter={() => setHovered(true)}
+                  onMouseLeave={() => setHovered(false)}
+                  href=""
+                  className=" md:text-xl text-white font-semibold inline-block"
+                >
+                  Discover GitHub Actions
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className={` text-white transition inline-block ml-3 ease-in duration-300 mb-[3px] ${
+                      hovered ? "translate-x-0 " : "-translate-x-1"
+                    }`}
+                    width="20"
+                    height="20"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M7.28033 3.21967C6.98744 2.92678 6.51256 2.92678 6.21967 3.21967C5.92678 3.51256 5.92678 3.98744 6.21967 4.28033L7.28033 3.21967ZM11 8L11.5303 8.53033C11.8232 8.23744 11.8232 7.76256 11.5303 7.46967L11 8ZM6.21967 11.7197C5.92678 12.0126 5.92678 12.4874 6.21967 12.7803C6.51256 13.0732 6.98744 13.0732 7.28033 12.7803L6.21967 11.7197ZM6.21967 4.28033L10.4697 8.53033L11.5303 7.46967L7.28033 3.21967L6.21967 4.28033ZM10.4697 7.46967L6.21967 11.7197L7.28033 12.7803L11.5303 8.53033L10.4697 7.46967Z"
+                    ></path>
+                    <path
+                      className={` text-white transition ease-in duration-150 ${
+                        hovered ? " opacity-100" : "opacity-0 "
+                      }`}
+                      stroke="currentColor"
+                      d="M1.75 8H11"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                    ></path>
+                  </svg>
+                  <div
+                    className={` ${
+                      hovered ? "w-11/12 scale-100" : "w-0 scale-0"
+                    } origin-left  transition ease-in duration-300 h-[2.5px] bg-white rounded-full`}
+                  ></div>
+                </a>
+              </div>
+            </div>
+
+            <div className="overflow-hidden rounded-s-lg">
+              <img
+                className="w-full h-auto js-build-in-item build-in-scale-fade build-in-animate"
+                width="1209"
+                height="890"
+                loading="lazy"
+                decoding="async"
+                alt=""
+                aria-hidden="true"
+                src="https://github.githubassets.com/images/modules/site/home-campaign/illu-actions.png"
+              />
+            </div>
+          </HoverCard>
+
+          <HoverCard
+            backgroundColor="#7ee787"
+            direction="flex-col"
+            left="-400px"
+          >
+            <div className="md:flex flex-col  flex-1 p-8 sm:p-10 lg:py-16 lg:pl-16 lg:pr-32 ">
+              <p className=" text-xl md:text-2xl mb-6 font-medium text-[#7d8590]">
+                <span className="text-white font-semibold">GitHub Mobile</span>{" "}
+                fits your projects in your pocket, so you never miss a beat
+                while on the go.
+              </p>
+
+              <div>
+                <a
+                  onMouseEnter={() => setHovered1(true)}
+                  onMouseLeave={() => setHovered1(false)}
+                  href=""
+                  className=" md:text-xl text-white font-semibold inline-block"
+                >
+                  Get GitHub Mobile
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className={` text-white transition inline-block ml-3 ease-in duration-300 mb-[3px] ${
+                      hovered1 ? "translate-x-0 " : "-translate-x-1"
+                    }`}
+                    width="20"
+                    height="20"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M7.28033 3.21967C6.98744 2.92678 6.51256 2.92678 6.21967 3.21967C5.92678 3.51256 5.92678 3.98744 6.21967 4.28033L7.28033 3.21967ZM11 8L11.5303 8.53033C11.8232 8.23744 11.8232 7.76256 11.5303 7.46967L11 8ZM6.21967 11.7197C5.92678 12.0126 5.92678 12.4874 6.21967 12.7803C6.51256 13.0732 6.98744 13.0732 7.28033 12.7803L6.21967 11.7197ZM6.21967 4.28033L10.4697 8.53033L11.5303 7.46967L7.28033 3.21967L6.21967 4.28033ZM10.4697 7.46967L6.21967 11.7197L7.28033 12.7803L11.5303 8.53033L10.4697 7.46967Z"
+                    ></path>
+                    <path
+                      className={` text-white transition ease-in duration-150 ${
+                        hovered1 ? " opacity-100" : "opacity-0 "
+                      }`}
+                      stroke="currentColor"
+                      d="M1.75 8H11"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                    ></path>
+                  </svg>
+                  <div
+                    className={` ${
+                      hovered1 ? "w-11/12 scale-100" : "w-0 scale-0"
+                    } origin-left  transition ease-in duration-300 h-[2.5px] bg-white rounded-full`}
+                  ></div>
+                </a>
+              </div>
+            </div>
+
+            <div className="overflow-hidden rounded-s-lg">
+              <img
+                className="w-full  h-auto"
+                width="1208"
+                height="764"
+                loading="lazy"
+                decoding="async"
+                alt=""
+                aria-hidden="true"
+                src="https://github.githubassets.com/images/modules/site/home-campaign/illu-mobile.png"
+              />
+            </div>
+          </HoverCard>
+        </div>
+      </div>
+
+      <motion.div
+        initial={{ height: 0 }}
+        whileInView={{ height: "160px" }}
+        transition={{ delay: 0.3 }}
+        viewport={{ once: false }}
+        className=" md:ml-10 ml-3 h-[160px] mt-[-20px] w-[3px] rounded-md bg-gradient-to-b from-transparent via-[#ea6045] to-[#ffa28b]"
+      ></motion.div>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 
+import Collaboration from "./components/Collaboration/Collaboration";
+import Globe from "./components/Footer/Globe";
 import Hero from "./components/Hero Section/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import StickyNav from "./components/Navbar/StickyNav";
 import Productivity from "./components/Productivity/Productivity";
+import Security from "./components/Security/Security";
+import Footer from "./components/Footer/Footer";
 
 const Home = () => {
   return (
@@ -28,11 +33,39 @@ const Home = () => {
             <Hero />
           </div>
 
-          <div className="px-4 pt-8 overflow-hidden mb-32" id="productivity">
+          <StickyNav />
+
+          <div className="px-4 pt-8 overflow-hidden mb-30" id="productivity">
             <Productivity />
+          </div>
+
+          <div className="px-4 pt-8 overflow-hidden" id="collaboration">
+            <Collaboration />
+          </div>
+
+          <div id="security" className="px-4 pt-8 pb-16 overflow-hidden">
+            <Security />
+          </div>
+
+          <Globe />
+
+          <div className="max-w-[1280px] mx-auto relative z-[2]  px-5">
+            <ul className="lg:w-9/12 text-[14px] pt-16 text-[#7d8590] space-y-1">
+              <li>
+                <sup id="footnote-1">1</sup> The Total Economic Impact™ Of
+                GitHub Enterprise Cloud and Advanced Security, a commissioned
+                study conducted by Forrester Consulting, 2022. Results are for a
+                composite organization based on interviewed customers.
+              </li>
+              <li>
+                <sup id="footnote-2">2</sup> GitHub, Octoverse 2022 The state of
+                open source software.
+              </li>
+            </ul>
           </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
