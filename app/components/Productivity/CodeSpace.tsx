f01cdbfce9a08a7c1489d56e24348479fe3a3700
KareemRoshdy <karem22roshdy@gmail.com>
2023-10-26 22:59:39 +0200

    final update

--- app/components/Productivity/CodeSpace.tsx
+++ app/components/Productivity/CodeSpace.tsx
@@ -3,18 +3,19 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 
-const CodeSpace = () => {
+type Props = {};
+
+const CodeSpace = (props: Props) => {
   const [hovered, setHovered] = useState<boolean>(false);
 
-  const lineHeight = {
+  const lineVariant = {
     hidden: { height: 0 },
     show: { height: "100%" },
   };
-
   return (
-    <div className="flex relative md:pl-5 ">
+    <div className="flex relative md:pl-10">
       <img
-        alt="codeSpaceImage"
+        alt=""
         aria-hidden="true"
         width="2712"
         height="2712"
@@ -23,19 +24,17 @@ const CodeSpace = () => {
         decoding="async"
         src="https://github.githubassets.com/images/modules/site/home-campaign/bg-stars-1.webp"
       />
-
       <div className="flex items-center flex-row relative">
         <motion.div
-          variants={lineHeight}
+          variants={lineVariant}
           initial="hidden"
           whileInView="show"
           viewport={{ once: false }}
           transition={{ duration: 0.3 }}
-          className="w-[3px] max-md:w-[2px] h-full increase origin-top"
+          className="w-[3px] h-full increase origin-top"
         ></motion.div>
-
         <img
-          alt="image"
+          alt=""
           aria-hidden="true"
           width="81"
           height="485"
@@ -51,9 +50,8 @@ const CodeSpace = () => {
           src="https://github.githubassets.com/images/modules/site/home-campaign/git-branch-productivity.svg"
         />
       </div>
-
-      <div className="lg:pl-24 md:pl-16 pl:6">
-        <div className="flex max-md:flex-col-reverse my-8 items-center lg:gap-x-5 sm:my-12 md:my-16 pd-6 md:pb-20 md:flex-row">
+      <div className="lg:pl-24 md:pl-16 pl-6">
+        <div className="flex max-md:flex-col-reverse my-8 items-center lg:gap-x-5 sm:my-12 md:my-16 pb-6 md:pb-20 md:flex-row">
           <div className="py-4 mb-2 w-full">
             <motion.div
               initial={{ opacity: 0, x: 30 }}
@@ -63,19 +61,18 @@ const CodeSpace = () => {
             >
               <h2 className="mb-4 text-2xl max-lg:text-[20px] max-md:w-10/12 lg:w-full w-full text-[#7d8590]">
                 <span className="font-semibold text-white">
-                  GitHub CodeSpace
+                  GitHub Codespaces
                 </span>{" "}
                 offers a complete dev environment in seconds, so you can code,
                 build, test, and open pull requests from any repo anywhere.
               </h2>
-
               <a
                 onMouseEnter={() => setHovered(true)}
                 onMouseLeave={() => setHovered(false)}
                 href=""
                 className="py-1 inline-block text-xl text-white font-semibold"
               >
-                Check out GitHub CodeSpaces
+                Check out GitHub Codespaces
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className={` mb-[2px] text-white transition inline-block ml-3 ease-in duration-300  ${
@@ -108,27 +105,25 @@ const CodeSpace = () => {
               </a>
             </motion.div>
           </div>
-
           <div className="py-4 px-6 w-full">
             <div className="relative">
               <div className="flex flex-row relative">
                 <motion.div
                   initial={{ opacity: 0, x: -30 }}
                   whileInView={{ opacity: 1, x: 0 }}
-                  transition={{ delay: 0.1, type: "tween" }}
+                  transition={{ type: "tween" }}
                   className="sm:absolute z-[1] top-[-200px] right-7 shadow-2xl"
                 >
                   <img
                     src="/pic.png"
-                    alt="image"
+                    alt=""
                     loading="lazy"
                     decoding="async"
                     className=" w-[300px] h-auto rounded-md"
                   />
-
                   <img
                     src="https://github.githubassets.com/images/modules/site/codespaces/illo-cursor.png"
-                    alt="image"
+                    alt=""
                     loading="lazy"
                     decoding="async"
                     className="absolute js-build-in-item build-in-slideX-left h-auto build-in-animate"
@@ -142,19 +137,18 @@ const CodeSpace = () => {
                     }}
                   />
                 </motion.div>
-
                 <motion.div
                   initial={{ opacity: 0, x: 30 }}
                   whileInView={{ opacity: 1, x: 0 }}
-                  transition={{ delay: 0.1, type: "tween" }}
-                  className="overflow-hidden max-w-[373px] bottom-0 rounded-md relative hidden sm:block build-in-animate"
+                  transition={{ type: "tween", delay: 0.1 }}
+                  className=" overflow-hidden max-w-[373px] bottom-0 rounded-md relative hidden sm:block build-in-animate"
                 >
                   <img
                     src="https://github.githubassets.com/images/modules/site/codespaces/illo-ports.png"
                     loading="lazy"
                     width="746"
                     height="368"
-                    alt="image"
+                    alt=""
                     decoding="async"
                     className="width-full d-block h-auto"
                   />
@@ -163,22 +157,19 @@ const CodeSpace = () => {
             </div>
           </div>
         </div>
-
         <motion.div
           initial={{ opacity: 0, x: -30 }}
           whileInView={{ opacity: 1, x: 0 }}
-          transition={{ delay: 0.1, type: "tween" }}
-          className="mb-10 md:mb-32 pb-4"
+          transition={{ type: "tween", delay: 0.1 }}
+          className="mb-10 md:mb-32 pb-4 "
         >
           <div className="py-4 text-left">
             <div className="z-[1] inline-block rounded-full px-2 border-2 border-[#7ee787] relative font-medium bg-gradient-to-r from-[#7ee787] to-[#aff5b4] bg-clip-text text-transparent">
               <span className="font-semibold text-[12px]">Did you know?</span>
             </div>
-
-            <h3 className="text-[#7ee787] mb-2 mt-3 lg:text-6xl md:text-5xl font-medium">
+            <h3 className="text-[#7ee787] mb-2 mt-3 lg:text-6xl md:text-5xl text-3xl font-medium">
               22% increase
             </h3>
-
             <p className="mb-3 md:text-xl text-base font-medium text-white">
               in developer productivity <br /> after three years with GitHub
               <sup>1</sup>
